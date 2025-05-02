3751ad2a1be5f5da0f5d9210485da6e7a5342aff
Okerachuchupurin <145318409+MashuNakamura@users.noreply.github.com>
2025-05-02 21:49:45 +0700

    Add Skeleton UI

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,18 +1,29 @@
+import React, { useState } from "react";
 import { Card, CardHeader, CardBody, Image, Link } from "@heroui/react";
-import React from "react";
-
-// Component to display a card view with an image and text
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export function CardView(): React.ReactElement {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <Link href="/detail">
       <Card className="py-4">
         <CardBody className="overflow-visible py-2">
+          {isLoading && (
+            <Skeleton
+              height={180}
+              width="100%"
+              style={{ borderRadius: "0.75rem", aspectRatio: "4 / 3" }}
+            />
+          )}
           <Image
             alt="Card background"
-            className="object-cover rounded-xl"
+            className={`rounded-xl object-cover w-full aspect-[4/3] ${
+              isLoading ? "hidden" : "block"
+            }`}
             src="https://app.requestly.io/delay/1000/https://heroui.com/images/hero-card-complete.jpeg"
-            width="100%"
+            onLoad={() => setIsLoading(false)}
           />
         </CardBody>
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
