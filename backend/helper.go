ac9690454edacc85ec164b9bdc097da3c33ff2a9
Fernando Perry <fernandoperry1234@gmail.com>
2025-05-30 13:52:25 +0700

    chore: move password related func to helper

--- backend/helper.go
+++ backend/helper.go
@@ -6,6 +6,8 @@ import (
 	"webrpl/table"
     "errors"
 	"gorm.io/gorm"
+    "fmt"
+    "golang.org/x/crypto/bcrypt"
 )
 
 func isEmailValid(e string) bool {
@@ -61,4 +63,19 @@ func getCredentialFromEnv() string {
         password = "secret"
     }
     return password
+}
+
+func HashPassword(password string) (string, error) {
+    // The cost parameter determines how computationally expensive the hash is to calculate
+    // The default is 10, but you can increase it for better security (at the cost of performance)
+    hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    if err != nil {
+        return "", fmt.Errorf("failed to hash password: %w", err)
+    }
+    return string(hashedBytes), nil
+}
+
+func CheckPassword(hashedPassword, plainPassword string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+    return err == nil
 }
