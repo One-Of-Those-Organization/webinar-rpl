3faac52fd99ab3f8134c85e8ec9f940173111366
Fernando Perry <fernandoperry1234@gmail.com>
2025-06-01 21:10:30 +0700

    fix: add checking if event-regist(desc, name, speaker) is empty.

--- backend/server_handler_event.go
+++ backend/server_handler_event.go
@@ -1,4 +1,5 @@
 package main
+// NOTE: Maybe need to change it to not check the jwt so not logged in people can get the webinar?
 
 import (
 	"encoding/base64"
@@ -84,12 +85,21 @@ func appHandleEventNew(backend *Backend, route fiber.Router) {
             EventLink: body.Link,
         }
 
+        if newEvent.EventDesc == "" || newEvent.EventName == "" || newEvent.EventSpeaker == "" {
+            return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+                "success": false,
+                "message": "Empty desc, name, speaker field is not allowed.",
+                "error_code": 5,
+                "data": nil,
+            })
+        }
+
         res = backend.db.Create(&newEvent)
         if res.Error != nil {
             return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
                 "success": false,
                 "message": fmt.Sprintf("Failed to create new event, %v", res.Error),
-                "error_code": 5,
+                "error_code": 6,
                 "data": nil,
             })
         }
