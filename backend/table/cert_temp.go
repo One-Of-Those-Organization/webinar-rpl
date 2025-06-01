120be77494bd9c09ccc92812d3a39ebbf95a0f7a
Fernando Perry <fernandoperry1234@gmail.com>
2025-06-01 17:18:07 +0700

    fix: fix the cert_temp table to include the path to the template.

--- backend/table/cert_temp.go
+++ backend/table/cert_temp.go
@@ -6,8 +6,9 @@ import (
 
 type CertTemplate struct {
     gorm.Model
-    CertId  int    `gorm:"column:cert_id;primaryKey"`
-    EventId int    `gorm:"column:event_id"`
+    CertId       int    `gorm:"column:cert_id;primaryKey"`
+    CertTemplate string `gorm:"column:cert_template"`
+    EventId      int    `gorm:"column:event_id"`
 
     Event   Event  `gorm:"foreignKey:EventId"`
 }
