89779e019646934322940a23f9c118cd720ed32c
MashuNakamura <federicomatthewpratamaa@gmail.com>
2025-06-12 01:37:00 +0700

    Chore: Change data type (i might be wrong)

--- src/api/interface.ts
+++ src/api/interface.ts
@@ -74,6 +74,7 @@ export class WebinarEdit {
   max?: number = 0;
   event_mat_id?: number = 0;
   cert_template_id?: number = 0;
+  panitia?: string = "";
 
   constructor(init?: Partial<WebinarEdit>) {
     Object.assign(this, init);
@@ -94,6 +95,7 @@ export class WebinarEdit {
       max: apiData.EventMax || 0,
       event_mat_id: apiData.EventMatID || 0,
       cert_template_id: apiData.CertTemplateID || 0,
+      panitia: apiData.Panitia || "",
     });
   }
 }
