2a30a1fad1a388cafed946ce712db5e4474a4358
Okerachuchupurin <145318409+MashuNakamura@users.noreply.github.com>
2025-06-22 02:53:48 +0700

    Fix: Bug on other browser (image)
    Add replace path (universal path) so it can be dynamic to other Browser
    OS
    Cause i use lenovo windows chrome , somehow it changed to 0.0.0.0
    instead of localhost:3000 (idk why) and that's make the image load
    broken

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -219,7 +219,8 @@ export default function ProfilPage() {
         const response = await auth_user.user_image({ data: base64Image });
 
         if (response.success) {
-          const serverPath = response.data?.filename || "";
+          let serverPath = response.data?.filename || "";
+          serverPath = serverPath.replace(/^https?:\/\/[^/]+:3000/, "http://localhost:3000");
           setUserData((prev) => ({ ...prev, profile: serverPath }));
           toast.success("Image uploaded! Remember to save your changes.");
         } else {
