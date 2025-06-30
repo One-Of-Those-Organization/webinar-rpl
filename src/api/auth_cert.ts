f15fe308e4e1dabf57f41d85f147a4a8e95ef38a
Fernando Perry <fernandoperry1234@gmail.com>
2025-06-30 20:17:12 +0700

    feat: add auth_cert.ts to call cert API.

---
+++ src/api/auth_cert.ts
@@ -0,0 +1,30 @@
+import { API_URL } from "@/api/endpoint";
+import {
+  BaseResponse,
+} from "./interface";
+
+export const auth_cert = {
+  create_cert: async (eventID: number): Promise<BaseResponse> => {
+    try {
+      const token = localStorage.getItem("token");
+      const data = JSON.stringify({
+        "event_id": Number(eventID),
+      })
+
+      const response = await fetch(`${API_URL}/api/protected/create-new-cert-from-event`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: data,
+      });
+      return await response.json();
+    } catch (error) {
+      return {
+        message: "Failed to connect to server",
+        success: false,
+      };
+    }
+  },
+}
